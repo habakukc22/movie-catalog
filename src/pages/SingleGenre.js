34136384da270e05875de012710f834c0bf76880
Habakuk <habakuk.conrado@gmail.com>
2022-12-09 16:32:14 -0300

    titl-of-singlePage

--- src/pages/SingleGenre.js
+++ src/pages/SingleGenre.js
@@ -1,24 +1,38 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import ListItem from "../components/MovieList/ListItem";
 import useHttp from "../hooks/useHttp";
+import { categoriesActions } from "../store/categories-slice";
 import classes from "./SingleGenre.module.css";
 
 function SingleGenre() {
   const { genreId } = useParams();
-  const categories = useSelector(state => state.categories.categories);
+  const dispatch = useDispatch();
+  const categories = useSelector((state) => state.categories.categories);
+
+  //fetch categories list and populate categories state case it's empty
+  let categoriesData = useHttp("/genre/movie/list?");
+  let genres = categoriesData ? categoriesData.genres : undefined;
+  if (genres && categories.length === 0) {
+    dispatch(categoriesActions.populateCategoryList(genres));
+  }
+
+  let movieGenreTitle = "";
+  for (let genre of categories) {
+    if (genre.id === +genreId) {
+      movieGenreTitle = genre.name;
+    }
+  }
 
   let data = useHttp(
     `/discover/movie?with_genres=${genreId}&sort_by=popularity.desc`
   );
   let fetchedMovies = data ? data.results : null;
-  console.log(data);
-  console.log(categories)
 
   if (fetchedMovies) {
     return (
       <>
-        <h1>{`Hey`}</h1>
+        <h1>{movieGenreTitle}</h1>
         <div className={classes.singleGenre}>
           {fetchedMovies.map((movie) => (
             <ListItem movie={movie} key={movie.id} />
