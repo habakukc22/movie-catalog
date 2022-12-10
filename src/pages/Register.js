c8365a54c158ce080fca5b1c8cc54840bb57d1cb
Habakuk <habakuk.conrado@gmail.com>
2022-12-10 14:35:31 -0300

    loginRegisterDone

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,50 @@
-// import classes from "./Register.module.css";
+import { Link } from "react-router-dom";
+import classes from "./Register.module.css";
 
 function Register() {
-  return <div>Register</div>;
+  const submitHandler = (e) => {
+    e.preventDefault();
+    alert("Submitted!");
+  };
+  return (
+    <div className={classes.registerContainer}>
+      <h2>Register</h2>
+
+      <form onSubmit={submitHandler}>
+        <div className={classes.inputField}>
+          <label htmlFor="first-name">First Name</label>
+          <input type="text" id="first-name" placeholder="First Name" />
+        </div>
+        
+        <div className={classes.inputField}>
+          <label htmlFor="first-last">Last Name</label>
+          <input type="text" id="first-last" placeholder="Last Name" />
+        </div>
+
+        <div className={classes.inputField}>
+          <label htmlFor="email">E-Mail</label>
+          <input type="email" id="email" placeholder="E-Mail" />
+        </div>
+
+        <div className={classes.inputField}>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" placeholder="Password" />
+        </div>
+        
+        <div className={classes.inputField}>
+          <label htmlFor="re-password">Re-enter password</label>
+          <input type="password" id="re-password" placeholder="Re-enter password" />
+        </div>
+
+        <button>Enter</button>
+      </form>
+
+      <div className={classes.login}>
+        <p>Do you already have an account? </p>
+        <Link to="/sign-in">Login!</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
