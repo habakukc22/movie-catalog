e7075b61eb441d54be991a86ea53577d6bcd3cc4
Habakuk <habakuk.conrado@gmail.com>
2022-12-15 10:40:07 -0300

    restrictionsProblemSolved

--- src/components/SingleMovie/RelatedMovies.jsx
+++ src/components/SingleMovie/RelatedMovies.jsx
@@ -3,6 +3,15 @@ import List from "../MovieList/List";
 function RelatedMovies(props) {
   let genres = props.movie.genres.map((el) => el.id).join(",");
 
+  let truncatedGenres = props.movie.genres
+    .map((el) => `${el.id}`)
+    .slice(0, 3)
+    .join(",");
+
+  if (genres.length > 3) {
+    genres = truncatedGenres;
+  }
+
   const category = {
     title: "Related popular movies",
     url: `/discover/movie?with_genres=${genres}&sort_by=popularity.desc`,
