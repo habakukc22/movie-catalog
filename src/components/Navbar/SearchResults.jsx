19e8594418191541337ddf4517faab457b12a46a
Habakuk <habakuk.conrado@gmail.com>
2022-12-17 08:45:46 -0300

    finished

--- src/components/Navbar/SearchResults.jsx
+++ src/components/Navbar/SearchResults.jsx
@@ -1,16 +1,26 @@
 import { useSelector } from "react-redux";
 import ListItem from "../MovieList/ListItem";
 import classes from "./SearchResults.module.css";
+import { ImSad } from "react-icons/im";
 
 function SearchResults() {
   let movies = useSelector((state) => state.search.searchedMovies);
+  console.log(movies);
 
   return (
-    <div className={classes.searchResults}>
-      {movies.map((movie) => (
-        <ListItem movie={movie} key={movie.id} />
-      ))}
-    </div>
+    <>
+      {movies.length === 0 && (
+        <div className={classes.error}>
+          <ImSad />
+          No results found...
+        </div>
+      )}
+      <div className={classes.searchResults}>
+        {movies.map((movie) => (
+          <ListItem movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </>
   );
 }
 
